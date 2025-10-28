230c7e78f8c7ea086d76f9d10b31e8c85c420a71
Kirill Molochinskiy <kirill9282@MacBook-Pro-Kirill-2.local>
2025-10-28 16:38:49 +0300

    Support MCU settings blocks and expose station status block 306

--- public/work/api.php
+++ public/work/api.php
@@ -1,3 +1,9 @@
 <?php
 
+$frontController = '/index.php';
+
+$_SERVER['SCRIPT_NAME']     = $frontController;
+$_SERVER['PHP_SELF']        = $frontController;
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../index.php';
+
 require __DIR__ . '/../index.php';
