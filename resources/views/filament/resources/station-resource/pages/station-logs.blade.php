4ba3828e96391bf93f66bae98756098e875713ac
Kirill Molochinskiy <kirill9282@MacBook-Pro-Kirill-2.local>
2025-10-30 15:16:18 +0300

    Обновления: журнал работ

--- resources/views/filament/resources/station-resource/pages/station-logs.blade.php
+++ resources/views/filament/resources/station-resource/pages/station-logs.blade.php
@@ -5,9 +5,9 @@
                 <tr>
                     <th class="border dark:border-gray-700 p-2 align-middle" rowspan="2">время</th>
                     <th class="border dark:border-gray-700 p-2 align-middle" rowspan="2">событие</th>
-                    <th class="border dark:border-gray-700 p-2 text-center" colspan="3">стир</th>
-                    <th class="border dark:border-gray-700 p-2 text-center" colspan="3">прогр</th>
-                    <th class="border dark:border-gray-700 p-2 text-center" colspan="3">бельё</th>
+                    <th class="border dark:border-gray-700 p-2 text-center" colspan="1">стир</th>
+                    <th class="border dark:border-gray-700 p-2 text-center" colspan="1">прогр</th>
+                    <th class="border dark:border-gray-700 p-2 text-center" colspan="1">бельё</th>
                     <th class="border dark:border-gray-700 p-2 text-center" colspan="8">стир. машины <br><span class="text-xs">(32 = 3 программа, 2 сигнал)</span></th>
                     <th class="border dark:border-gray-700 p-2 text-center" colspan="8">моющие средства</th>
                     <th class="border dark:border-gray-700 p-2 align-middle" rowspan="2">комментарий</th>
