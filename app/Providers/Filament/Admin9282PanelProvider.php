041fbeda8ddb92705ccd5374af15aa3fcfa20370
Kirill Molochinskiy <kirill9282@MacBook-Pro-Kirill-2.local>
2025-11-03 14:07:03 +0300

    Обновления: [опиши, что изменил]ывсывсывс

--- app/Providers/Filament/Admin9282PanelProvider.php
+++ app/Providers/Filament/Admin9282PanelProvider.php
@@ -40,6 +40,7 @@ class Admin9282PanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->viteTheme('resources/css/app.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
