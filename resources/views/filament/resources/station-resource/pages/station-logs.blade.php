9f1f0fcaa02832191b0d260fe782824a88ef85a8
Kirill Molochinskiy <kirill9282@MacBook-Pro-Kirill-2.local>
2025-11-03 14:42:27 +0300

    Обновления: [опиши, что изменил]ывсывсывс

--- resources/views/filament/resources/station-resource/pages/station-logs.blade.php
+++ resources/views/filament/resources/station-resource/pages/station-logs.blade.php
@@ -6,8 +6,8 @@
             <label class="text-sm font-medium">Масштаб таблицы</label>
             <input
                 type="range"
-                min="0.6"
-                max="1"
+                min="0.5"
+                max="2"
                 step="0.05"
                 x-model.number="tableScale"
                 class="w-48"
@@ -98,6 +98,8 @@
         {{-- Пагинация --}}
         <div class="p-4">
             {{ $this->getLogs()->links() }}
+        </div>
+            </div>
         </div>
     </div>
 </x-filament-panels::page>
